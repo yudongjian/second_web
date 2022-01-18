89a110340aa5dc8dbfa46f9a46074a742f48f624
tyler <tyler@163.com>
2022-01-18 22:04:36 +0800

    update upload file js code

--- untitledtest/app1/test2.py
+++ untitledtest/app1/test2.py
@@ -1,25 +1,13 @@
-import threading
-import time
+def my_decorator(func):
+    def wrapper(*args, **kwargs):
+        '''decorator'''
+        print('Calling decorated function...')
+        return func(*args, **kwargs)
+    return wrapper
 
-def run():
-    time.sleep(2)
-    print('当前线程的名字是： ', threading.current_thread().name)
-    time.sleep(2)
+@my_decorator
+def example():
+    """Docstring"""
+    print('Called example function')
 
-
-if __name__ == '__main__':
-
-    start_time = time.time()
-
-    print('这是主线程：', threading.current_thread().name)
-    thread_list = []
-    for i in range(5):
-        t = threading.Thread(target=run)
-        thread_list.append(t)
-
-    for t in thread_list:
-        t.start()
-
-    print('主线程结束！' , threading.current_thread().name)
-    t.join()
-    print('一共用时：', time.time()-start_time)
+print(example.__name__, example.__doc__)
